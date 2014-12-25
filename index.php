9e7ebb11005b71b6dd809f238a2d5730500a81bd
Moritz Schmidt <fusselwurm@gmail.com>
2014-12-25 11:34:59 +0100

    how-to in description

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*
 * Plugin Name: Fucking Simple Gallery
-* Description: Hacky as hell. For it to work: include underscorejs, execute fusselGalleryHack() somewhere on page. Also, I deactivated the colorbox call in wp-content/plugins/grid-gallery-ready/src/GirdGallery/Galleries/views/shortcode/gallery.twig . Yikes!
+* Description: Hacky as hell. For it to work: include underscorejs, execute <code>fusselGalleryHack()</code> somewhere on page. For it to work with the <em>Grid Gallery</em>: deactivate the colorbox call in <code>wp-content/plugins/grid-gallery-ready/src/GirdGallery/Galleries/views/shortcode/gallery.twig</code> , call <code>fusselGalleryHack</code> in <code>wp-content/plugins/grid-gallery-ready/src/GirdGallery/Galleries/assets/js/frontend.js</code> after gallery initialization
 * Version: 0.1
 * Author: Fusselwurm
 * License: GPLv2
